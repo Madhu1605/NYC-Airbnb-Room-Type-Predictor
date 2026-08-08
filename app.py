07904d9d59a9d7caf65e0a24549afbfe6372a3a6
Madhu1605 <madhugarugu16@gmail.com>
2026-08-08 23:17:41 +0530

    connect streamlit to deployed fastapi

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide"
 )
 
-API_URL = "http://127.0.0.1:8000/predict"
+API_URL = "https://nyc-airbnb-room-type-predictor-wbmp.onrender.com/predict"
 
 
 # =========================================================
